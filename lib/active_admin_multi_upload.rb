d5ba0e5cfe94725d876b5c95801e6910e4032de1
Yuriy Chernov <yurgon.com@gmail.com>
2015-03-27 23:54:29 +0300

    wtf?

--- lib/active_admin_multi_upload.rb
+++ lib/active_admin_multi_upload.rb
@@ -1,7 +1,2 @@
 require "active_admin_multi_upload/engine"
 require "active_admin_multi_upload/dsl"
-
-module ActiveAdminMultiUpload
-end
-
-::ActiveAdmin::DSL.send(:include, ActiveAdminMultiUpload::DSL)
